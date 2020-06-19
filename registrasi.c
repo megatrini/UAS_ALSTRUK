afbc011ec9ca091cd18b86d9ad2c0bb2730a9cbd
Mega Trini Oktaviani <62134888+megatrini@users.noreply.github.com>
2020-06-19 15:06:07 +0700

    Create registrasi.c

---
+++ registrasi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+char nama[100],namab[100],email[500], u[100],hp[150];
+
+
+int main(){
+    printf("Masukan Nama Depan : ");
+    scanf("%s",&nama);
+    
+    printf("Masukan Nama Belakang : ");
+    scanf("%s",&namab);
+    
+	printf("Masukan Email :  ");
+    scanf("%s",&email);
+    
+    printf("Masukan usia: ");
+    scanf("%d",&u);
+    
+    printf("Masukan no.hp: ");
+    scanf("%d",&hp);
+    
+ 	return 0;
+	
+	
+}
